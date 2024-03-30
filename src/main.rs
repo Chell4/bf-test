754dc64341b312680745a234cac1091be2f1018c
chell4 <arsenijalfeev@gmail.com>
2024-03-30 17:44:09 +0300

    rm shmain

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,3 @@
-mod shmain;
-
 fn main() {
     println!("Man I Like Fishing!");
-
-    shmain::shmain();
-    println!("called fuction shmain::shmain()");
 }
--- src/shmain.rs
+++
@@ -1,3 +0,0 @@
-pub fn shmain() {
-    println!("aaaaa");    
-}
